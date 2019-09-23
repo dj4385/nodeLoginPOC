a2b331b39e54bce5fa9845e2404a1727282b5d82
dheeraj sharma <dj4385@gmail.com>
2019-09-23 13:03:17 +0530

    cors is added in app.js

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const express = require('express'),
 
 // app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json()) // with this you can send the data to api in json format
-
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.get('/',(req,res)=>{
     res.send({
