dbc7533a337b6719c1bd808cc240a30f22e31156
dheeraj sharma <dj4385@gmail.com>
2019-11-13 11:44:31 +0530

    db url change

--- config/configFile.js
+++ config/configFile.js
@@ -1,5 +1,5 @@
 module.exports = {
     "serverPort": process.env.PORT || 4000, // to set process to env -> PORT=1234 node app.js
-    //"dbPath": "mongodb+srv://dheeraj:dheeraj123@cluster0-8tivr.mongodb.net/test?retryWrites=true&w=majority"
-    "dbPath": "mongodb://localhost:27017/userDataBase"
+    "dbPath": "mongodb+srv://dheeraj:dheeraj123@cluster0-8tivr.mongodb.net/test?retryWrites=true&w=majority"
+    //"dbPath": "mongodb://localhost:27017/userDataBase"
 }
